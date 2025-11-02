70f24c45034e95e7f5035b3a77400fa63388f997
fadygouda <fadygouda123@gmail.com>
2025-11-02 12:48:30 -0800

    fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     cout << "You have entered into the Eight Puzzle solver." << endl;
     cout << "You can either use a pre-built puzzle or enter your own. Type 1 for a pre-built puzzle, or 2 to enter your own!" << endl;
-    int selection
+    int selection;
 
     
 }
