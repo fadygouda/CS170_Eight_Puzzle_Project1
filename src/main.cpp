9217a6cf9c0e14f473c82442bb2946ae7132d336
fadygouda <fadygouda123@gmail.com>
2025-11-02 17:35:15 -0800

    created terminal prompting

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,37 @@ int main() {
     cout << "You can either use a pre-built puzzle or enter your own. Type 1 for a pre-built puzzle, or 2 to enter your own!" << endl;
     int selection;
 
+    cin >> selection;
+
+    cout << "Choose which search algorighm you would like to use: " << endl;
+    cout << "1) Uniform Cost Search" << endl;
+    cout << "2) A* with Misplaced Tile heuristic" << endl;
+    cout << "3) A* with Euclidean Distance heuristic" << endl;
+    
+    int algorithm_selection;
+    cin >> algorithm_selection;
+
+    if(algorithm_selection == 1) {
+        cout << "Uniform Cost Search selected." << endl;
+    } else if (algorithm_selection == 2) {
+        cout << "A* with Misplaced Tile heuristic selected." << endl;
+    } else if (algorithm_selection == 3) {
+        cout << "A* with Euclidean Distance heuristic selected" << endl;
+    } else {
+        cout << "Invalid Selection, you can only select 1-3" << endl;
+        return 0;
+    }
+    
+
+
+    
+    
+
+
+
+
+
+
+
     
 }
