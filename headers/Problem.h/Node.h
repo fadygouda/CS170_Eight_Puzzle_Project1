19b026ccf4ea8e53a920047c154d58ab87d0b329
marcencarnacion <74811291+marcencarnacion@users.noreply.github.com>
2025-11-03 16:16:29 -0800

    Update Node.h

--- headers/Problem.h/Node.h
+++ headers/Problem.h/Node.h
@@ -2,35 +2,44 @@
 
 #include <vector>
 #include <string>
+#include <utility>
 using namespace std;
 
 class Node {
 public:
+    // Struct to represent the blank position.
+    // struct Position {
+    //     int row;
+    //     int col;
+    // };
 
+    //Node(const vector<vector<int>>& game_state, int path_cost, int heuristicCost, Node* parent, const Position& blank_position, const string& chosen_action) : state(game_state), path_cost(path_cost), heuristic(heuristicCost), parent(parent), blank_pos(blank_position), action(chosen_action), depth(0) {} 
 
-    int heuristic;
-    struct Position {
-        int row;
-        int col;
-    };
+    Node (const vector<vector<int>>& gridState,
+    int pathCost,
+    int heuristicCost,
+    Node* parentNode,
+    const pair<int, int>& blankTile,
+    const string& moveUsed);
 
-    Node(const vector<vector<int>>& game_state, int path_cost, int heuristicCost, Node* parent, const Position& blank_position, const string& chosen_action) : state(game_state), path_cost(path_cost), heuristic(heuristicCost), parent(parent), blank_pos(blank_position), action(chosen_action), depth(0) {} 
-
-    bool is_goal(const vector<vector<int>>& goal) const {
-        return state == goal;
-    }
-    int get_heuristic() const;
-    int get_total_cost() const;
+    //this will return true if the current board is the goal board
+    bool is_goal(const vector<vector<int>>& goalState) const;
+    //getter for g cost
     int get_path_cost() const;
+    //getter for h cost
+    int get_heuristic() const;
+    //f = g+h
+    int get_total_cost() const;
+    //getting the state
     const vector<vector<int>>& get_state() const;
-
-    vector<Node*> add_child(const vector<string>& actions);
+    vector<Node*> add_child(const vector<string>& moves);
 
 private:
-    vector<vector<int>> state;
-    int path_cost;                 
-    int depth;          
-    string action;          
-    Node* parent;        
-    Position blank_pos;       
+    vector<vector<int>> grid;//Layout of the puzzle
+    int gValue;              //Cost from the start
+    int hValue;              //Heuristic value
+    int depth = 0;           
+    string moveMade;         //Moved used to reach node    
+    Node* parent;            //Pointer to parent
+    pair<int, int> blankPos; //Blank tile coords (row, column)
 };
